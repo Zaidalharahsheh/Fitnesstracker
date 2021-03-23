309854a7247a142e5278061f75b0726b92cbe52b
Zaidalharahsheh <zalharahsheh@gmail.com>
2021-03-22 21:26:22 -0400

    k

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fittrack",
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false
-  }
+  },
 { useNewUrlParser: true });
 
 //require('./seeders/seed')
